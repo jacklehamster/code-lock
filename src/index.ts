fa042f621d87fd5de91f30083d7bed4b0c0e9ab0
Jack Le Hamster <jacklehamster@gmail.com>
2024-06-27 02:08:51 -0700

    Commit Summary: Updated entrypoints, changed target in bundler.ts, updated module path in package.json, removed index.test.ts, added index.ts and removed index.tsx
    - In `bundler.ts`, changed the entrypoints from './src/index.tsx' to './src/index.ts'.
    - Could use a more descriptive comment to explain why the entrypoint was changed, something like "Changed entrypoint to match new file extension for index file".
    
    - In `bundler.ts`, changed the target from "browser" to "node".
    - Who knew a simple change in target could change a file's destiny!
    
    - In `package.json`, updated the module from "src/index.tsx" to "src/index.ts".
    - Let's just hope this change doesn't cause module mayhem!
    
    - Removed `src/index.test.ts`.
    - Bye-bye test file! Did it fail to test its own existence?
    
    - Added `src/index.ts`, a new file.
    - Welcome to the jungle, new file!
    
    - Removed `src/index.tsx`.
    - Goodbye old friend, or should I say old file!
    
    Developer Joke: Why was the developer always calm during emergencies? Because he handled "git" with everything!
    
    Files:
    M  bundler/bundler.ts
    M  package.json
    D  src/index.test.ts
    A  src/index.ts
    D  src/index.tsx
    
    Committed using auto-commit: Thu Jun 27 2024 02:08:51 GMT-0700 (Pacific Daylight Time)
    Autocommit project: https://www.npmjs.com/package/@dobuki/autocommit
    All the lame jokes were generated by AI.

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { Lock } from "./interface/Lock";
+import { RedisLock } from "./redislock/redislock";
+import { SimpleLock } from "./simplelock/simplelock";
+
+export { Lock, RedisLock, SimpleLock };
